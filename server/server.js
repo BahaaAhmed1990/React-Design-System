0655f183a8ce0c30fa9d367c51eebe956b78e906
unknown <bahaaahmed19902012@gmail.com>
2024-08-01 06:56:25 +0300

    test production mode for language logic

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ if (mode == "story") {
   app.use(express.static(path.resolve(__dirname, "../front/storybook-static")));
 } else {
   console.log("production mode");
+  // Middleware to set CORS headers
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", `http://127.0.0.1:${port}`);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
   app.use(express.static(path.resolve(__dirname, "../front/dist")));
 }
 
